567c9c79953c12360166faebd7d724d804c40eba
Arnie Hayes <47344710+Ninerpack@users.noreply.github.com>
2021-04-10 23:01:28 -0700

    for u son

--- FinalProject/movie_desc.js
+++ FinalProject/movie_desc.js
@@ -1,8 +1,12 @@
+//window.onload = localStorage.getItem("storageName");
+
 const APIURL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+  "https://api.themoviedb.org/3/movie/" +
+  localStorage.getItem("storageName") +
+  "?api_key=94f2d3081ba573d2f171f0f8020eb38a";
+IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+  "https://api.themoviedb.org/3/search/movie?&api_key=94f2d3081ba573d2f171f0f8020eb38a&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -24,61 +28,66 @@ function showMovies(movies) {
   // clear main
   main.innerHTML = "";
 
-  movies.forEach((movie) => {
-    const { poster_path, title, overview } = movie;
+  console.log(movies)
 
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
+  //const { poster_path, title, overview, id } = movies;
 
-      movieEl.innerHTML = `
-        <div class = "image-video">
-            <div class="image-window">
-                <img class = "image" src="${IMGPATH + poster_path}">
-            </div>
-            <div class="youtube-window">
-                <iframe class = "video" src="https://www.youtube.com/embed/ORFWdXl_zJ4">
-                </iframe>
-            </div>
-        </div>
+  for (i = 0; i < 1; i++) {
+     const { poster_path } = i;
 
-        <div class = "genre-service">
-            <div class = "genre-container">
-                <h1 class = "sect-title">Genre</h1>
-                <div class="genre-card">             
-                    <div class=""></div>
-                    <p class = "card-text genre-service-text">Animation, Comedy</p>
-                  </div>
-            </div>
-            <div class= "service-container">
-                <h1 class = "sect-title">Streaming Service</h1>
-                <div class="service-card">             
-                    <div class=""></div>
-                    <p class = "card-text genre-service-text">Disney+</p>
-                  </div>
-            </div>
-        </div>
+     const movieEl = document.createElement("div");
+     movieEl.classList.add("movie");
 
+     movieEl.innerHTML = `
+  <div class = "image-video">
+          <div class="image-window">
+              <img class = "image" src="${IMGPATH + poster_path}">
+          </div>
+          <div class="youtube-window">
+              <iframe class = "video" src="">
+              </iframe>
+          </div>
+      </div>
 
-        <div class = "description">
-                <h1 class = "sect-title">Description</h1>
-                <div class="card">
-                    <div class ="">            
-                    <p class = "card-text">Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.</p>
+      <div class = "genre-service">
+          <div class = "genre-container">
+              <h1 class = "sect-title">Genre</h1>
+              <div class="genre-card">             
+                  <div class=""></div>
+                  <p class = "card-text genre-service-text">Animation, Comedy</p>
                 </div>
+          </div>
+          <div class= "service-container">
+              <h1 class = "sect-title">Streaming Service</h1>
+              <div class="service-card">             
+                  <div class=""></div>
+                  <p class = "card-text genre-service-text">Disney+</p>
                 </div>
-        </div>
+          </div>
+      </div>
 
-        <div class = "cast">
-            <h1 class = "sect-title">Cast</h1>
-                <div class="card">   
-                    <div class ="">       
-                    <p class = "card-text">Edward Asner, Christopher Plummer, Jordan Nagai, Bob Peterson, Delroy Lindo, Jerome Ranft</p>
-                    </div>   
-                </div>
-        `;
 
-    main.appendChild(movieEl);
-  });
+      <div class = "description">
+              <h1 class = "sect-title">Description</h1>
+              <div class="card">
+                  <div class ="">            
+                  <p class = "card-text">Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.</p>
+              </div>
+              </div>
+      </div>
+
+      <div class = "cast">
+          <h1 class = "sect-title">Cast</h1>
+              <div class="card">   
+                  <div class ="">       
+                  <p class = "card-text">Edward Asner, Christopher Plummer, Jordan Nagai, Bob Peterson, Delroy Lindo, Jerome Ranft</p>
+                  </div>   
+              </div>
+      `;
+
+     main.appendChild(movieEl);
+  }
+
 }
 
 form.addEventListener("submit", (e) => {
