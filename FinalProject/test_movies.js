369453e3c6c393b7b5d692a0ee211c1f42509fa7
leto42 <54646724+leto42@users.noreply.github.com>
2021-04-10 20:49:26 -0700

    Replace navbar

--- FinalProject/test_movies.js
+++ FinalProject/test_movies.js
@@ -1,8 +1,8 @@
 const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=94f2d3081ba573d2f171f0f8020eb38a&page=1";
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=94f2d3081ba573d2f171f0f8020eb38a&query=";
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -25,7 +25,7 @@ function showMovies(movies) {
     main.innerHTML = "";
 
     movies.forEach((movie) => {
-        const { poster_path, title, vote_average, overview } = movie;
+        const { poster_path, title, overview } = movie;
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
@@ -37,9 +37,6 @@ function showMovies(movies) {
             />
             <div class="movie-info">
                 <h3>${title}</h3>
-                <span class="${getClassByRate(
-                    vote_average
-                )}">${vote_average}</span>
             </div>
             <div class="overview">
                 <h3>Overview:</h3>
@@ -49,16 +46,6 @@ function showMovies(movies) {
 
         main.appendChild(movieEl);
     });
-}
-
-function getClassByRate(vote) {
-    if (vote >= 8) {
-        return "green";
-    } else if (vote >= 5) {
-        return "orange";
-    } else {
-        return "red";
-    }
 }
 
 form.addEventListener("submit", (e) => {
