3cb0f62987a6a683ee5ddde42fdda27283cdc5cf
Arnie Hayes <47344710+Ninerpack@users.noreply.github.com>
2021-04-10 21:26:31 -0700

    for thomas

---
+++ FinalProject/movie_desc.js
@@ -0,0 +1,94 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  console.log(respData);
+
+  showMovies(respData.results);
+}
+
+function showMovies(movies) {
+  // clear main
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { poster_path, title, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+      movieEl.innerHTML = `
+        <div class = "image-video">
+            <div class="image-window">
+                <img class = "image" src="${IMGPATH + poster_path}">
+            </div>
+            <div class="youtube-window">
+                <iframe class = "video" src="https://www.youtube.com/embed/ORFWdXl_zJ4">
+                </iframe>
+            </div>
+        </div>
+
+        <div class = "genre-service">
+            <div class = "genre-container">
+                <h1 class = "sect-title">Genre</h1>
+                <div class="genre-card">             
+                    <div class=""></div>
+                    <p class = "card-text genre-service-text">Animation, Comedy</p>
+                  </div>
+            </div>
+            <div class= "service-container">
+                <h1 class = "sect-title">Streaming Service</h1>
+                <div class="service-card">             
+                    <div class=""></div>
+                    <p class = "card-text genre-service-text">Disney+</p>
+                  </div>
+            </div>
+        </div>
+
+
+        <div class = "description">
+                <h1 class = "sect-title">Description</h1>
+                <div class="card">
+                    <div class ="">            
+                    <p class = "card-text">Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.</p>
+                </div>
+                </div>
+        </div>
+
+        <div class = "cast">
+            <h1 class = "sect-title">Cast</h1>
+                <div class="card">   
+                    <div class ="">       
+                    <p class = "card-text">Edward Asner, Christopher Plummer, Jordan Nagai, Bob Peterson, Delroy Lindo, Jerome Ranft</p>
+                    </div>   
+                </div>
+        `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+    search.value = "";
+  }
+});
